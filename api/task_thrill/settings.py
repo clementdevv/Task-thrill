951970f8849a45db9d114d3008421cdf5d5df1ae
clementdevv <clementdevv@gmail.com>
2025-01-12 15:39:39 +0300

    added venv

--- api/task_thrill/settings.py
+++ api/task_thrill/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-3&$*u1xzeh64j-oddsw4$j+alznm45wwzobbwkz*qu&rtg+d-g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['clementdevv.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
